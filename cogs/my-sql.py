2033913ae766a37b8ecdcaa950c47284b309576b
ad1tyajava <link.aditya.java@gmail.com>
2024-10-19 01:06:49 +0530

    Create my-sql.py

---
+++ cogs/my-sql.py
@@ -0,0 +1,7 @@
+# sql functions
+
+# create timestamp - last update time
+
+# check timstamp - read timestamp file agar current next day hai toh return false else true
+
+# write aur read ke funcitons
